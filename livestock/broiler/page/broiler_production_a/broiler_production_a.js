22df1b3c3a10a037fc70b38e87ba7aced57ff13b
jayan13 <jayakumar13s@gmail.com>
2023-07-12 00:54:21 +0530

    report broiler production

---
+++ livestock/broiler/page/broiler_production_a/broiler_production_a.js
@@ -0,0 +1,106 @@
+
+frappe.pages['broiler-production-a'].on_page_load = function(wrapper) {
+	new MyPage(wrapper);
+}
+
+MyPage =Class.extend({
+	
+	init: function(wrapper){
+			this.page = frappe.ui.make_app_page({
+			parent: wrapper,
+			title: 'Broiler Production & Sales Monthly Comparison Report',
+			single_column: true
+		});
+			this.make();
+	},
+	make: function()
+	{
+		const comarry=[];
+		var date = new Date();
+		var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+		var firstDay = new Date(date.getFullYear(), date.getMonth()-2, 1);
+			let field = this.page.add_field({
+			label: 'Company',
+			fieldtype: 'Link',
+			fieldname: 'company',
+			options: 'Company',
+			change() {
+				get_report();
+			},
+			default:'ABU DHABI MODERNE POULTRY FARM L.L.C.'
+		});
+		
+		let field1 = this.page.add_field({
+			label: 'Date',
+			fieldtype: 'Date',
+			fieldname: 'date_from',
+			change() {
+				
+				get_report();
+				
+			}
+		});
+
+		let field2 = this.page.add_field({
+			label: 'Date',
+			fieldtype: 'Date',
+			fieldname: 'date_to',
+			change() {
+				
+				get_report();
+				
+			}
+		});
+		
+		field1.set_value(firstDay);
+		field2.set_value(lastDay);		
+		
+		this.page.add_inner_button('Print', () => print_rep());
+		let data='';
+		$(frappe.render_template("broiler_production_a",data)).appendTo(this.page.main);
+		
+		function get_report()
+		{
+			$('#report_egg').html('');
+			if(field.get_value())
+			{		
+					frappe.call({
+					method: 'livestock.broiler.page.broiler_production_a.broiler_production_a.get_report',
+					freeze: 1,
+					freeze_message: 'Data loading ...please waite',
+					args: {
+					  company: field.get_value(),
+					  date_from: field1.get_value(),
+					  date_to: field2.get_value(),
+					},
+					callback: function (r) {
+					  if (r.message) {
+						  $('#report_egg').html(r.message);
+							
+							
+					  }
+					},
+				  });
+	  
+			}
+		}
+		
+		
+		
+	}
+
+
+})
+
+function print_rep()
+		{
+
+			  var divrear=document.getElementById('report_egg');
+					
+					  var newWin=window.open('','Print-Window');
+					  newWin.document.open();
+					  newWin.document.write('<html><style>table, th, td {border: 1px solid;border-collapse: collapse; } table{ width:100%;} table td{ text-align:right;} table td:first-child{ text-align:left;} .table-secondary td,.table-secondary th {background-color: #d5d7d9;font-weight: bold;} #rephd{ font-size: 18px; font-weight: bold; padding: 15px;}  @media print { #prod{overflow-x:unset !important;} #rer{overflow-x:unset !important;} } </style><body onload="window.print()">'+divrear.innerHTML+'</body></html>');
+					  newWin.document.close();
+					  setTimeout(function(){newWin.close();},10);
+  
+		}
--- livestock/broiler/page/broiler_production_a/broiler_production_a.py
+++ livestock/broiler/page/broiler_production_a/broiler_production_a.py
@@ -1,20 +1,16 @@
 import frappe
 from frappe.utils import getdate,add_days,get_first_day,get_last_day,nowdate,flt,date_diff
-@frappe.whitelist()
-def get_company_list():
-    data = {}
-    data["companys"] = frappe.get_list("Company", fields=['name'],limit_page_length=0, order_by="name",debug=0)
-    return data
+
 
 @frappe.whitelist()
 def get_report(company,date_from,date_to):
     date_from=getdate(date_from)
     date_to=getdate(date_to)
     date_range=[] #['ORG1115','ORG1130'] ['ORG EGG','ORG EGG BROWN']
-    items=frappe.db.get_all("Item",filters={'item_group':'EGGS','item_code':['NOT in',['ORG EGG','ORG EGG BROWN']]},fields=['item_code'],order_by='item_code',pluck='item_code')
+    itemgp=['CHICKEN PRODUCTS - ACACIA','CHICKEN PRODUCTS - AL FAKHER','CHICKEN PRODUCTS - AUH']
+    items=frappe.db.get_all("Item",filters={'item_group':'EGGS','item_group':['in',['CHICKEN PRODUCTS - ACACIA','CHICKEN PRODUCTS - AL FAKHER','CHICKEN PRODUCTS - AUH']]},fields=['item_code'],order_by='item_code',pluck='item_code')
     items_str="','".join(items)
-    itemss=items
-    itemss_str=items_str
+    
 
     cnt=0
     day = date_from
@@ -38,19 +34,20 @@ def get_report(company,date_from,date_to):
     proddatetot=[]
     range_res=[]
     if date_range:
-        html+='<tr class="table-secondary"><th style="width: 225px;">Items </th>'
+        html+='<tr class="table-secondary"><th style="width: 250px;">Items </th>'
         for dr in date_range:
-            html+='<th style="text-align:center;font-weight:bold">'+str(frappe.utils.formatdate(dr.get('from'), "MMM yy"))+'(Qty in Ctn)</th>'
+            html+='<th style="text-align:center;font-weight:bold">'+str(frappe.utils.formatdate(dr.get('from'), "MMM yy"))+'(Weight in Kg)</th>'
             proddatetot.append(0)
-            resprd=frappe.db.sql(""" SELECT sum(qty*conversion_factor) as qty,`tabStock Entry Detail`.item_code 
+            resprd=frappe.db.sql(""" SELECT sum(qty*conversion_factor*weight_per_unit) as qty,weight_per_unit,`tabStock Entry Detail`.item_code 
                     FROM `tabStock Entry Detail`
-                    JOIN `tabStock Entry` on `tabStock Entry Detail`.parent = `tabStock Entry`.name
+                    left JOIN `tabStock Entry` on `tabStock Entry Detail`.parent = `tabStock Entry`.name
+                    left JOIN `tabItem` on `tabItem`.name = `tabStock Entry Detail`.item_code
                     WHERE
                     `tabStock Entry`.docstatus=1
                     AND  `tabStock Entry Detail`.item_code in ('{0}')
-                    AND ((`tabStock Entry Detail`.is_finished_item = 1
+                    AND `tabStock Entry Detail`.is_finished_item = 1
                     AND `tabStock Entry`.stock_entry_type = 'Manufacture'
-                    AND `tabStock Entry`.manufacturing_type = 'Egg') or (`tabStock Entry`.stock_entry_type = 'Material Receipt' AND `tabStock Entry`.stock_entry_type_option='Production') )
+                    AND `tabStock Entry`.manufacturing_type = 'Chicken Slaughtering'
                     AND `tabStock Entry`.company = '{1}'
                     AND `tabStock Entry`.posting_date between '{2}' 
                     AND '{3}'
@@ -64,15 +61,13 @@ def get_report(company,date_from,date_to):
                 d=0
                 i=0
                 for dr in date_range:
-                    #if itm.item_code=='CG11111':
-                     #   d=1
-                    qtyinctn=0
+                    
                     itmqty=range_res[i]
                     if itmqty:
                         for it in itmqty:
                             if itm==it.item_code:
                                 if it.qty:
-                                    qtyinctn=float(it.qty)/360
+                                    qtyinctn=it.qty
 
                     html+='<td class="text-right">'+str(frappe.utils.fmt_money(flt(qtyinctn,3)))+'</td>'
 
@@ -98,26 +93,26 @@ def get_report(company,date_from,date_to):
     netqty_tot=[]
     netsale_tot=[]
     if date_range:
-        html+='<tr class="table-secondary"><th rowspan="2" style="width: 225px;">Items </th>'
+        html+='<tr class="table-secondary"><th  style="width: 250px;position: sticky;top: 0;">Items </th>'
         for dr in date_range:
-            html+='<th colspan="6" style="text-align:center;font-weight:bold">'+str(frappe.utils.formatdate(dr.get('from'), "MMM yy"))+'</th>'
+            html+='<th colspan="6" style="text-align:center;font-weight:bold;position: sticky;top: 0; ">'+str(frappe.utils.formatdate(dr.get('from'), "MMM yy"))+'</th>'
             saleqtytot.append(0)
             saleamttot.append(0)
             salerqtytot.append(0)
             saleramttot.append(0)
             netqty_tot.append(0)
             netsale_tot.append(0)
-            ressale=frappe.db.sql(""" SELECT `tabSales Invoice Item`.item_code,ROUND(sum(`tabSales Invoice Item`.base_net_amount),2) as amt,sum(qty*conversion_factor) as qty
+            ressale=frappe.db.sql(""" SELECT `tabSales Invoice Item`.item_code,ROUND(sum(`tabSales Invoice Item`.base_net_amount),2) as amt,sum(total_weight) as qty
                     FROM `tabSales Invoice`
-                    JOIN `tabSales Invoice Item` on `tabSales Invoice Item`.parent = `tabSales Invoice`.name
+                    JOIN `tabSales Invoice Item` on `tabSales Invoice Item`.parent = `tabSales Invoice`.name                   
                     WHERE `tabSales Invoice`.docstatus=1
                     AND `tabSales Invoice`.is_return!=1
                     AND `tabSales Invoice Item`.item_code in('{0}')
                     AND `tabSales Invoice`.company = '{1}'
                     AND `tabSales Invoice`.posting_date between '{2}' AND '{3}'
-                    GROUP BY `tabSales Invoice Item`.item_code """.format(itemss_str,company,dr.get('from'),dr.get('to')),as_dict=1,debug=0)
+                    GROUP BY `tabSales Invoice Item`.item_code """.format(items_str,company,dr.get('from'),dr.get('to')),as_dict=1,debug=0)
             range_ress.append(ressale)
-            ressaler=frappe.db.sql(""" SELECT `tabSales Invoice Item`.item_code,ROUND(sum(`tabSales Invoice Item`.base_net_amount),2) as amt,sum(qty*conversion_factor) as qty
+            ressaler=frappe.db.sql(""" SELECT `tabSales Invoice Item`.item_code,ROUND(sum(`tabSales Invoice Item`.base_net_amount),2) as amt,sum(total_weight) as qty
                     FROM `tabSales Invoice`
                     JOIN `tabSales Invoice Item` on `tabSales Invoice Item`.parent = `tabSales Invoice`.name
                     WHERE `tabSales Invoice`.docstatus=1
@@ -125,17 +120,17 @@ def get_report(company,date_from,date_to):
                     AND `tabSales Invoice Item`.item_code in ('{0}')
                     AND `tabSales Invoice`.company = '{1}'
                     AND `tabSales Invoice`.posting_date between '{2}' AND '{3}'
-                    GROUP BY `tabSales Invoice Item`.item_code """.format(itemss_str,company,dr.get('from'),dr.get('to')),as_dict=1,debug=0)
+                    GROUP BY `tabSales Invoice Item`.item_code """.format(items_str,company,dr.get('from'),dr.get('to')),as_dict=1,debug=0)
             range_ressr.append(ressaler)
         html+='</tr>'
 
-        html+='<tr class="table-secondary">'
+        html+='<tr class="table-secondary"><th></th>'
         for dr in date_range:
-            html+='<th style="position: unset;font-weight:normal;">Qty</th><th>Amount</th><th>Rtn Qty</th><th>Rtn Amount</th><th>Net Qty</th><th>Net Amt</th>'            
+            html+='<th >Weight kg</th><th>Amount</th><th>Rtn Weight kg</th><th>Rtn Amount</th><th>Net Weight</th><th>Net Amt</th>'            
         html+='</tr>'
 
-        if itemss:
-            for itm in itemss:
+        if items:
+            for itm in items:
                 html+='<tr>'
                 html+='<td>'+str(getitem_name(itm))+'</td>'
                 d=0
@@ -157,7 +152,7 @@ def get_report(company,date_from,date_to):
                         for it in itmqty:
                             if itm==it.item_code:
                                 if it.qty:
-                                    qtyinctn=float(it.qty)/360
+                                    qtyinctn=it.qty
                                 amount=it.amt
                     html+='<td class="text-right" '+str(sty)+'>'+str(frappe.utils.fmt_money(flt(qtyinctn,3)))+'</td>'
                     html+='<td class="text-right" '+str(sty)+'>'+str(frappe.utils.fmt_money(amount))+'</td>'
@@ -169,7 +164,7 @@ def get_report(company,date_from,date_to):
                         for ret in salret:
                             if itm==ret.item_code:
                                 if ret.qty:
-                                    srqty=float(ret.qty)/360
+                                    srqty=ret.qty
                                 srval=ret.amt
 
                     html+='<td class="text-right" '+str(sty)+'>'+str(frappe.utils.fmt_money(flt(srqty,3)))+'</td>'
@@ -214,70 +209,7 @@ def get_report(company,date_from,date_to):
         
     html+='</table></div>'
 
-    #================== sales return ===========
-    html+='<div class="rephd">Sales Return: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;From Date: '+str(frappe.utils.formatdate(date_from))+' To Date : '+str(frappe.utils.formatdate(date_to))+'</div>' 
-    html+='<div class="scrl" ><table class="table table-bordered" >'
-    saleqtytot=[]
-    saleamttot=[]
     
-    if date_range:
-        html+='<tr class="table-secondary"><th rowspan="2" style="width: 225px;">Items </th>'
-        for dr in date_range:
-            html+='<th colspan="2" style="text-align:center;font-weight:bold">'+str(frappe.utils.formatdate(dr.get('from'), "MMM yy"))+'</th>'
-            saleqtytot.append(0)
-            saleamttot.append(0)
-            
-        html+='</tr>'
-
-        html+='<tr class="table-secondary">'
-        for dr in date_range:
-            html+='<th>Qty</th><th>Amount</th>'            
-        html+='</tr>'
-
-        if itemss:
-            for itm in itemss:
-                html+='<tr>'
-                html+='<td>'+str(getitem_name(itm))+'</td>'
-                d=0
-                i=0
-                for dr in date_range:
-                    #if itm.item_code=='CG11111':
-                     #   d=1
-                    if ((i+1) % 2) == 0:                        
-                        sty=' style="background-color: #f3faff; border: 1px solid #ccc;" '
-                        
-                        
-                    else:                        
-                        sty=' style=" border: 1px solid #ccc;" '
-                        
-
-                    qtyinctn=0
-                    amount=0
-                    itmqty=range_ressr[i]
-                    if itmqty:
-                        for it in itmqty:
-                            if itm==it.item_code:
-                                if it.qty:
-                                    qtyinctn=float(it.qty)/360
-                                amount=it.amt
-                    html+='<td class="text-right" '+str(sty)+' >'+str(frappe.utils.fmt_money(flt(qtyinctn,3)))+'</td>'
-                    html+='<td class="text-right" '+str(sty)+' >'+str(frappe.utils.fmt_money(amount))+'</td>'
-
-                    tot=saleqtytot[i]
-                    saleqtytot[i]=float(tot)+float(qtyinctn)
-                    tots=saleamttot[i]
-                    saleamttot[i]=float(tots)+float(amount)    
-                    i+=1
-                html+='</tr>'
-            html+='<tr class="table-secondary"><th>Total</th>'
-            j=0
-            for total in saleqtytot:
-                 html+='<th class="text-right"><b>'+str(frappe.utils.fmt_money(flt(total,3)))+'</b></th>'
-                 html+='<th class="text-right"><b>'+str(frappe.utils.fmt_money(flt(saleamttot[j],3)))+'</b></th>'
-                 j+=1
-            html+='</tr>'
-        
-    html+='</table></div>'
 
     return html
 
